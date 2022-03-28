17924ab2be8a0b26208db91ba79ff005c6c1579f
TypicalOperator <81247022+TypicalOperator@users.noreply.github.com>
2022-03-28 19:18:09 +0300

    Update NormalAnimation.lua

--- NormalAnimation.lua
+++ NormalAnimation.lua
@@ -1,1 +1,99 @@
+_G.AnimationSpeed = 1
+function Move(Part, Type, X, Y, Z)
+    spawn(function()
+    if Type == "Position" or "Pos" then
+        Part.Position = Vector2.new(X, Y, Z)
+    elseif Type == "Rotation" or "Rot" then
+        Part.Orientation = Vector3.new(X, Y, Z)
+    end
+end)
+end
+function Animate(Part, Type, X, Y, Z)
+    spawn(function()
+    if Type == "Position" or "Pos" then
+        local part = Part
 
+
+
+
+
+local TweenService = game:GetService("TweenService")
+
+
+
+
+
+local tweenInfo = TweenInfo.new(
+
+
+
+	AnimationSpeed, --Time
+
+	Enum.EasingStyle.Linear, --Easing Style
+
+	Enum.EasingDirection.Out, --EasingDirection
+
+    0, --Repeat Count
+
+	true, --Reverse
+
+	0 --DelayTime
+
+
+
+)
+
+
+
+
+
+local tween = TweenService:Create(part, tweenInfo, {Position = UDim2.new(X, Y, z)})
+
+
+
+tween:Play()
+    elseif Type == "Rotation" or "Rot" then
+        local part = Part
+
+
+
+
+
+local TweenService = game:GetService("TweenService")
+
+
+
+
+
+local tweenInfo = TweenInfo.new(
+
+
+
+	AnimationSpeed, --Time
+
+	Enum.EasingStyle.Linear, --Easing Style
+
+	Enum.EasingDirection.Out, --EasingDirection
+
+    0, --Repeat Count
+
+	true, --Reverse
+
+	0 --DelayTime
+
+
+
+)
+
+
+
+
+
+local tween = TweenService:Create(part, tweenInfo, {Orientation = Vector3.new(X, Y, Z)})
+
+
+
+tween:Play()
+    end
+end)
+end
